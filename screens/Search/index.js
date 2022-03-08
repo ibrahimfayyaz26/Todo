41142a44014f94b5f3163dbd68af7806b773e666
Ibrahim <79158349+ibrahimfayyaz26@users.noreply.github.com>
2022-03-08 23:18:07 +0500

    data

--- screens/Search/index.js
+++ screens/Search/index.js
@@ -1,11 +1,96 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, TextInput, FlatList} from 'react-native';
+import React, {useState} from 'react';
 import styles from './styles';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import recent from '../../Assets/data/recent.json';
 
 const Search = () => {
+  const [active, setActive] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Search</Text>
+      <View style={styles.header}>
+        <TextInput
+          placeholder="Find Friends"
+          style={styles.textInput}
+          onFocus={() => setActive(true)}
+          onEndEditing={() => setActive(false)}
+        />
+        <EvilIcons name="search" size={40} color="#533E85" />
+      </View>
+      {active ? (
+        <View>
+          <FlatList
+            data={recent}
+            keyExtractor={item => item.id}
+            renderItem={({item}) => {
+              return (
+                <View
+                  style={{
+                    padding: 10,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                  }}>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                    }}>
+                    <EvilIcons name="search" size={35} color="#533E85" />
+
+                    <Text
+                      style={{
+                        fontSize: 19,
+                        color: '#FFC7C7',
+                        fontFamily: 'Abel-Regular',
+                        fontWeight: 'bold',
+                        marginLeft: 10,
+                      }}>
+                      {item.recent}
+                    </Text>
+                  </View>
+                  <AntDesign
+                    style={{marginRight: 5}}
+                    name="select1"
+                    size={25}
+                    color="#533E85"
+                  />
+                </View>
+              );
+            }}
+          />
+        </View>
+      ) : (
+        <View>
+          <FlatList
+            data={recent}
+            keyExtractor={item => item.id}
+            renderItem={({item}) => {
+              return (
+                <View
+                  style={{
+                    padding: 10,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}>
+                  <AntDesign name="close" size={25} color="#533E85" />
+                  <Text
+                    style={{
+                      fontSize: 19,
+                      color: '#FFC7C7',
+                      fontFamily: 'Abel-Regular',
+                      fontWeight: 'bold',
+                      marginLeft: 10,
+                    }}>
+                    {item.recent}
+                  </Text>
+                </View>
+              );
+            }}
+          />
+        </View>
+      )}
     </View>
   );
 };
--- screens/Search/styles.js
+++ screens/Search/styles.js
@@ -4,6 +4,23 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#8785A2',
     flex: 1,
+    margin: 5,
+    padding: 5,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderWidth: 1,
+    borderRadius: 10,
+    width: '100%',
+  },
+  textInput: {
+    marginLeft: 10,
+    fontSize: 18,
+    width: '80%',
+    color: '#FFC7C7',
+    fontFamily: 'Abel-Regular',
   },
 });
 
